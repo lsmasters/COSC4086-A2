c3b25c13ed37cca78356948d2487cc7fa027dd26
lsmasters <lmasters@algomau.ca>
2025-05-15 15:07:12 +0000

    added user.php

---
+++ user.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'database.php';
+
+Class User {
+  public function get_all_users() {
+    $db = db_connect();
+    $sql =
+    $stmt = $db->prepare( "SELECT * FROM users;");
+    $stmt->execute();
+    $rows = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $rows;
+  }
+}
+
+?>
